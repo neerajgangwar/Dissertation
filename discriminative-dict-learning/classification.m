f5a3076e8e4b4c1166c8023b8ef909a4d48b0150
Neeraj Gangwar <y.neeraj2008@gmail.com>
2014-05-15 10:40:35 +0530

    added a new function classification which takes 1. Dictionary 2. Test samples 3. Class information as input and gives classification information as output.

--- discriminative-dict-learning/classification.m
+++ discriminative-dict-learning/classification.m
@@ -1,20 +1,40 @@
-D = Di;
-G = D'*D;
-gamma = computeSparseCodes(A_test, D);
-pos = 0;
-neg = 0;
+function [H_est, H_act, C, recognition_rate] = classification(D, A, H)
 
-for u = 1 : size(gamma, 2)
-    a = gamma(:, u);
-    estClass = estimateClass(A_test(:, u), D, a, 38);
-    [C, actualClass] = max(H_test(:, u));
-    
-    if actualClass == estClass
-        pos = pos + 1;
-    else
-        neg = neg + 1;
-    end
-    
+	% function classifies the test samples w.r.t. dictionary D
+	% Input:
+	%		D : discriminative dictionary
+	%		A : N x M matrix with M test samples of dimension N.
+	% 		H : Label information of all samples.
+	% Output:
+	%		H_est : A vector of size M x 1 with estimated classes of each test sample.
+	%		H_act : A vector of size M x 1 with actual classes of each test sample.
+	%		C : Confusion matrix
+	%		recognition_rate : Recognition rate (%)
+
+	H_act = [];
+	H_est = [];
+
+	atoms_per_class = ceil(size(D, 2)/size(H, 1));
+	gamma = compute_sparse_codes(A, D);
+	pos = 0;
+	neg = 0;
+
+	for u = 1 : size(gamma, 2)
+	    a = gamma(:, u);
+	    est_class = face_classification(D, A(:, u), a, atoms_per_class);
+	    [C, actual_class] = max(H(:, u));
+	    
+	    H_est = [H_est est_class];
+	    H_act = [H_act actual_class]
+
+	    if actual_class == est_class
+	        pos = pos + 1;
+	    else
+	        neg = neg + 1;
+	    end
+	    
+	end
+
+	C = confusionmat(H_act, H_est);
+	recognition_rate = pos*100/(pos + neg);
 end
-
-fprintf([num2str(pos) ' , ' num2str(neg) ' , ' num2str(pos/(pos+neg)) '\n']);
