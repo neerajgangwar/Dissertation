a9fc050e0846204fe18cb88239f206b307e9be0d
Neeraj Gangwar <y.neeraj2008@gmail.com>
2014-06-10 09:59:56 +0530

    removed lc-ksvd directory as the code is available online

--- lc-ksvd/normcols.m
+++
@@ -1,3 +0,0 @@
-function matout=normcols(matin)
-l2norms = sqrt(sum(matin.*matin,1)+eps);
-matout = matin./repmat(l2norms,size(matin,1),1);
--- lc-ksvd/start_spams.m
+++
@@ -1,6 +0,0 @@
-addpath('spams-matlab/test_release');
-addpath('spams-matlab/src_release');
-addpath('spams-matlab/build');
-setenv('MKL_NUM_THREADS','1')
-setenv('MKL_SERIAL','YES')
-setenv('MKL_DYNAMIC','NO')
